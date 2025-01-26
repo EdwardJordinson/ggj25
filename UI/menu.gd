fd38c6a833f7c3e97da96104109c2b9d1789e28a
Krish Dubey <122972264+Krish-Dubey@users.noreply.github.com>
2025-01-26 15:17:05 +0800

    Credits Fixed
    The credits button in options works now

--- UI/menu.gd
+++ UI/menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var mainMenu : Control = $MarginContainer
 @onready var settings : Control = $MarginContainer2
+@onready var credits : Control = $MarginContainer3
 
 func _on_play_pressed() -> void:
 	self.visible=false
@@ -17,8 +18,8 @@ func _on_quit_pressed() -> void:
 
 
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
-
+	settings.visible=false
+	credits.visible=true
 
 func _on_back_pressed() -> void:
 	mainMenu.visible=true
@@ -27,3 +28,8 @@ func _on_back_pressed() -> void:
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("menu_button"):
 		self.visible=true
+
+
+func _on_back_2_pressed() -> void:
+	settings.visible=true
+	credits.visible=false
